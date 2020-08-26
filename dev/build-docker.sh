e84f59923d13c8922887aeb6d07faae2caf873f7
refracta <58779799+refracta@users.noreply.github.com>
2020-08-26 10:30:50 +0900

    Update build-docker.sh

--- dev/build-docker.sh
+++ dev/build-docker.sh
@@ -5,7 +5,6 @@ sudo docker build -t emalron/nethack ./dev
 sudo docker run -it \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e DISPLAY=unix$DISPLAY \
-    -v /tmp:/tmp \
     -v "$PWD":/usr/src/nethack \
 	-p 80:80 \
     --name nethack-webtiles-dev \
